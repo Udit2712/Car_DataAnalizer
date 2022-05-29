91a830eec2ae3c6caa9c016673f2b22f4d26f1be
Udit2712 <78135334+Udit2712@users.noreply.github.com>
2022-05-29 21:52:07 +0530

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -4,6 +4,13 @@ $username = "uhnbcyoa0gynhczg";
 $password = "QoD888qNbDN812WfCndP";
 $dbname = "bxlqioeaf2nxdeugfemn";
 
+
+//FOR RUNNING ON LOCAL HOST
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "cars_engage";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
